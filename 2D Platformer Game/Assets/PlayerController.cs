90fb54be8dcaba1882182986c5fd7fb87c59a7da
PoWer <PoWer@users.noreply.github.com>
2020-04-13 12:47:20 +0200

    Player Controll 1
    Can move left and right speed 3

---
+++ 2D Platformer Game/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-3, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(3, 0);
+        }
+    }
+}
