60b25a2f04819f84ee19018dd18ca211a82a48f8
PoWer <PoWer@users.noreply.github.com>
2020-04-13 12:52:28 +0200

    Player Controll 2
    Jumping [5]

--- 2D Platformer Game/Assets/PlayerController.cs
+++ 2D Platformer Game/Assets/PlayerController.cs
@@ -10,12 +10,17 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector2(-3, 0);
+            rb.velocity = new Vector2(-3, rb.velocity.y);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector2(3, 0);
+            rb.velocity = new Vector2(3, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5);
         }
     }
 }
