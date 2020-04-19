f328be8d73d5363cac337111e8e70034ae950c1a
PoWer <PoWer@users.noreply.github.com>
2020-04-19 20:03:12 +0200

    Problem Fixed
    Koden för att spelaren ska vända på sig gör att kameran inte fungerar

--- 2D Platformer Game/Assets/PlayerController.cs
+++ 2D Platformer Game/Assets/PlayerController.cs
@@ -11,13 +11,11 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-4, rb.velocity.y);
-            transform.localScale = new Vector2(-1, 1);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(4, rb.velocity.y);
-            transform.localScale = new Vector2(1, 1);
         }
 
         if (Input.GetKey(KeyCode.Space))
