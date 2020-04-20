2948fa99697c4e5eee35fdadfa24a31dc7a7299a
PoWer <PoWer@users.noreply.github.com>
2020-04-20 08:52:56 +0200

    Animation 1
    Byter mellan "run" och "idle"

--- 2D Platformer Game/Assets/PlayerController.cs
+++ 2D Platformer Game/Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
 
     private void Update()
     {
@@ -12,12 +13,19 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(-4, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(4, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
+        }
+
+        else
+        {
+            anim.SetBool("running", false);
         }
 
         if (Input.GetKey(KeyCode.Space))
